aec7a8e04e323dab0a03b765997d195a225e8f09
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:54:30 -0300

    Implement ClienteService for client management

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -1,1 +1,22 @@
 
+package service;
+
+import model.Cliente;
+import repository.ClienteRepository;
+
+public class ClienteService {
+
+    private ClienteRepository repository = new ClienteRepository();
+
+    public void cadastrarCliente(String nome, String email) {
+
+        Cliente cliente = new Cliente(nome, email);
+        repository.adicionarCliente(cliente);
+
+        System.out.println("Cliente cadastrado com sucesso!");
+    }
+
+    public void listarClientes() {
+        repository.listarClientes();
+    }
+}
