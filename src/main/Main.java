8415f99235b8d6601dd465b50bb5634a388dd19c
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:55:05 -0300

    Add main class to register and list clients

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,1 +1,19 @@
 
+package main;
+
+import service.ClienteService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ClienteService service = new ClienteService();
+
+        service.cadastrarCliente("Augusto", "augusto@email.com");
+        service.cadastrarCliente("Maria", "maria@email.com");
+
+        System.out.println("Lista de clientes:");
+
+        service.listarClientes();
+    }
+}
