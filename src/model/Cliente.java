f66e412821e2edb85d4fa67007d4c13c52bc5a4d
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:53:25 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,1 +1,25 @@
+package model;
 
+public class Cliente {
+
+    private String nome;
+    private String email;
+
+    public Cliente(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void exibirCliente() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+    }
+}
