76c13b9f76b8e0434defdc42030317bdb2bc1fd8
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:54:01 -0300

    Update ClienteRepository.java

--- src/repository/ClienteRepository.java
+++ src/repository/ClienteRepository.java
@@ -1,1 +1,22 @@
 
+package repository;
+
+import model.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteRepository {
+
+    private List<Cliente> clientes = new ArrayList<>();
+
+    public void adicionarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public void listarClientes() {
+        for (Cliente cliente : clientes) {
+            cliente.exibirCliente();
+            System.out.println("-------------");
+        }
+    }
+}
